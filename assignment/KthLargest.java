6e369fae7728c6a0c81fb3cb0c2a4a7746cff99d
Bin Shi <i@shibin.info>
2017-09-16 11:29:06 -0700

    PDF is ready

--- assignment/KthLargest.java
+++ assignment/KthLargest.java
@@ -1,36 +1,31 @@
-//Assignment: Find the 3rd largest number in an unsorted array.
-
 package assignment;
 
-public class KthLargest {
+//Assignment: Find the 3rd largest number in an unsorted array.
 
+import java.util.Arrays;
+public class KthLargest {
 	public static int find(int[] nums, int k) {
 		if (nums == null || nums.length == 0)
 			return Integer.MAX_VALUE;
-
 		return find(nums, 0, nums.length - 1, nums.length - k);
 	}
 
-	private static int find(int[] nums, int start, int end, int k) {
-		k = Math.min(k, nums.length);
-
+	private static int find(int[] nums, int start, int end, int n) {
+		n = Math.min(n, nums.length);
 		assert start <= end;
-
 		int pivot = nums[end];
 		int left = start;
 		for (int i = start; i < end; i++) {
 			if (nums[i] <= pivot)
 				swap(nums, left++, i);
 		}
-
 		swap(nums, left, end);
-
-		if (left == k)
+		if (left == n)
 			return nums[left];
-		else if (left < k)
-			return find(nums, left + 1, end, k);
+		else if (left < n)
+			return find(nums, left + 1, end, n);
 		else
-			return find(nums, start, left - 1, k);
+			return find(nums, start, left - 1, n);
 	}
 
 	private static void swap(int[] nums, int i, int j) {
@@ -38,10 +33,11 @@ public class KthLargest {
 		nums[i] = nums[j];
 		nums[j] = tmp;
 	}
-
+	
 	public static void main(String[] args) {
 		int k = 3;
-		int[] numbers = { 7, 4, 7, 4, 9, 1, 4, 1, 9, 10 };
+		int[] numbers = { 4, 1, 9, 10 };
 		System.out.println(find(numbers, k));
+		System.out.println(Arrays.toString(numbers));
 	}
 }
--- assignment/KthLargest_Arrays.java
+++ assignment/KthLargest_Arrays.java
@@ -3,9 +3,16 @@ package assignment;
 import java.util.Arrays;
 
 public class KthLargest_Arrays {
-
 	public static int find(int[] nums, int k) {
+		int n = Math.min(k, nums.length);
 		Arrays.sort(nums);
-		return nums[nums.length - k];
+		return nums[nums.length - n];
 	}
+	
+	public static void main(String[] args) {
+		int k = 3;
+		int[] numbers = { 2,1 };
+		System.out.println(find(numbers, k));
+	}
+	
 }
